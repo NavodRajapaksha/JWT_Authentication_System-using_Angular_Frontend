e9f5a70913cbdf8de6793251aa03f085940018fc
Navod D Rajapaksha <129612778+NavodRajapaksha@users.noreply.github.com>
2026-04-16 23:41:49 +0530

    feat : added user ts & user session localsesions

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -3,23 +3,43 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Menu } from '../../services/menu';
+import { RouterLink } from "@angular/router";
+import { UserAuth } from '../../services/user-auth';
+import { NgIf } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
   imports: [
-    MatToolbarModule, 
-    MatButtonModule, 
-    MatIconModule],
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+    NgIf
+],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
 
-  constructor(private menu: Menu) {
+  constructor(
+    private menu: Menu,
+    private userAuth: UserAuth,
+    private router: Router
+  ) {
   }
 
   toggleMenu() {
     this.menu.toggle();
   }
 
+  public isLoggedIn() {
+    return this.userAuth.isLoggedIn();
+  }
+
+  public logout() {
+    localStorage.clear();
+    this.router.navigate(['/home']);
+  } 
 }
--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { OnInit } from '@angular/core';
 import { User } from '../../services/user';
+import { UserAuth } from '../../services/user-auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,18 +15,31 @@ import { User } from '../../services/user';
 })
 export class Login implements OnInit {
   
-  constructor(private userService: User) { }
+  constructor(
+    private userService: User,
+    private UserAuth:UserAuth,
+    private router: Router 
+  ) { }
 
   ngOnInit(): void {
   }
 
   login( loginForm: NgForm) {
-    console.log(loginForm.value);
-    console.log("Login successful");
+    // console.log(loginForm.value);
+    // console.log("Login successful");
 
     this.userService.login(loginForm.value).subscribe(
-      (response) => {
-        console.log("Login successful", response);
+      (response: any) => {
+
+        this.UserAuth.setRoles(response.userEntity.roleEntitySet);
+        this.UserAuth.setToken(response.jwtToken);
+
+        const role =  response.userEntity.roleEntitySet[0].roleName;
+        if(role === 'Admin') {
+          this.router.navigate(['/admin']);
+        }else{
+          this.router.navigate(['/user']);
+        }
       },
       (error) => {
         console.error("Login failed", error);
